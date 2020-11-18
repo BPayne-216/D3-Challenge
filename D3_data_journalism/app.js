e7c3c16ec722666c19eec3105debc5957bf38079
Robert Payne <rjp5473@yahoo.com>
2020-11-17 23:05:50 -0500

    Commit part I continued

--- D3_data_journalism/app.js
+++ D3_data_journalism/app.js
@@ -19,14 +19,42 @@ var svg = d3
   .attr("width", svgWidth)
   .attr("height", svgHeight)
   .append("g")
-  .attr("transform", `translate(${margin.left}, ${margin.top})`);
+  .attr("transform", `translate(${margin.left}, ${margin.top})`)
+
 var chart = svg.append("g"); 
+
+d3.select(".scatter").append("div").attr("class","tooltip").style("opacity",0);
 
 //  Retrieve data from the CSV file and execute everything below
 d3.csv("data.csv", (function(err, demographicData) {
     if (err) throw err;
 
     demographicData.forEach(function(data) {
-        
-    })
-}
+      data.poverty = +data.poverty;
+      data.healthcare = +data.healthcare; 
+    });
+    //  Scale y and x to chart height and width
+    var yScale = d3.scaleLinear().range([height,0]);
+    var xScale = d3.scaleLinear().range([0,width]);
+
+    //  Create initial axis functions
+    var yAxis = d3.axisLeft(yScale);
+    var xAxis = d3.axisBottom(xScale);
+    //  Create y and x scale function
+    yScale.domain([0, d3.max(demographicData, function(data)  {
+        return +data.healthcare;
+        }),
+    ]);
+    xScale.domain([9, d3.max(demographicData, function(data) {
+        return +data.poverty;
+        }),
+    ]);
+
+    var toolTip = d3.tip()
+      .attr("class", "tooltip")
+      .offset([80,-60])
+      .html(function(data) {
+
+      })
+    }
+
